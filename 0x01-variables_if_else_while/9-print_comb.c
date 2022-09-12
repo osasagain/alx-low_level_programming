be553138e6c4ed388bb5e7a6f8215a305bb70b38
osasagain <osasogbokhun@gmail.come>
2022-09-12 11:00:09 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,20 @@
 int main(void)
 {
 	int c;
+	
 	for (c = '0'; c <= '9' c++)
 	{
-		int c;
-		for (c = '0'; c <= '9'; c++)
+	
+	 	putchar(c);
+	
+		if(c != '9')
 		{
-			putchar(c);
-			if(c != '9')
-			{
-				putchar(',');
-				putchar('');
-			}
+			putchar(',');
+			putchar('');
 		}
-		putchar('\n');
-		return (0);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
