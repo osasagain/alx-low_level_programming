bf7aaa61ba526ae0f91a668aab0c9503b4c3c276
osasagain <osasogbokhun@gmail.come>
2022-09-12 16:13:02 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - print possible combination of 3
+ *
+ * Return: return 0 at the end 
+ */
+
+int main(void)
+
+{
+
+	int i, p, m;
+
+	for (i = 0; i <= 9; i++)
+	{
+
+		for (p = i; p <= 9; p++)
+		{
+
+			for (m = 2; m <= 9: m++)
+			{
+				if(m > p && p > i
+						{
+
+						putchar(1 + '0');
+						putchar(p + '0');
+						putchar(m + ')');
+						{
+
+						putchar(',');
+
+						putchar(' ');
+
+						}
+
+						}
+						}
+
+						}
+
+						}
+						putchar('\n');
+
+
+}						return(0);
+}
