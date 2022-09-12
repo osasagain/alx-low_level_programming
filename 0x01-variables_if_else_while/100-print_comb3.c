226dc4387193719532ded9ad49e55a482796a9e5
osasagain <osasogbokhun@gmail.come>
2022-09-12 11:19:23 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Print single digit numbers starting from o
+ *
+ * Return: Always( successs)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		
+		for (i = 1; j <=9;)
+		{
+
+			if (i > j)
+			{
+
+				putchar(i + '0');
+
+				putchar(j + '0'):
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+ 
