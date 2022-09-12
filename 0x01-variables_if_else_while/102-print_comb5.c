deea48bfcaf11d546400cb3c02962faf05d468cc
osasagain <osasogbokhun@gmail.come>
2022-09-12 16:46:36 -0700

    Task 3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,14 +8,22 @@
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z' ; ch++)
 	{
+
 		putchar(ch);
+
 	}
+
 	for (ch = 'A' ; ch <= 'z' ; ch++)
 	{
+
 		putchar(ch);
+
 	}
+
 	putchar('\n');
+
 	return(0);
 }
