1f9aac684afce524686f420f670e03c1f4ed0ebf
osasagain <osasogbokhun@gmail.come>
2022-09-12 15:37:03 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,19 +5,30 @@
  * Return: Always (success)
  */
 int main(void)
+
 {
+
 	int a;
 
-	for(a= 0; a < 10; a++)
+	for(a = 0; a < 10; a++)
+
 	{
 		putchar(a + '0');
 
+
 		if (a != 9);
+		
 		{
+		
 			putchar(',');
+		
 			putchar(' ');
 		}
+	
 	}
+	
 	putchar('\n');
+	
 	return (0);
+
 }
