d41c5fb83a4f6f62ecabb82740386b274c0e6a44
osasagain <osasogbokhun@gmail.come>
2022-09-14 11:03:10 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: This program prints _putchar
+ *
+ * Return: 0
+ * */
+int main(void)
+
+{
+
+	-putchar('_');
+	-putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+programming function in C
