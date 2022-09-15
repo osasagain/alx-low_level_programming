0c946613895945fb6b42b780535ae3c0774e4bc0
osasagain <osasogbokhun@gmail.come>
2022-09-14 19:21:12 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Desription: This program print alphabet x10
+ */
+void print_alphabet_x10(voidd)
+
+{
+
+	char alphabet;
+	int count;
+
+	count 0;
+	while (count != 10)
+	{
+
+		for (alphabet = 'a'; alphabet <= 'z' alphabet++)
+		{
+
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+		count++;
+	}
+}
