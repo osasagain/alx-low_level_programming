a64646220fd947371eaed4d3c782a91eafdfbe96
osasagain <osasogbokhun@gmail.come>
2022-09-14 18:55:53 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point 
+ *
+ * Description: Function to print alphabet 
+ *
+ * Return: Always 0 (successful)
+ */
+void print_alphabety(void)
+{
+
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z' alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	_return('\n');
+}
