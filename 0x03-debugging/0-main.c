db6a406c947619245bc602853d8365cf52361bea
osasagain <osasogbokhun@gmail.come>
2022-09-16 07:57:13 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main- entry point
+ *
+ * Description: prints positive or negartive integer
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int i;
+	i =0;
+	positive_or_negative(i);
+
+	return(0);
+
+}
