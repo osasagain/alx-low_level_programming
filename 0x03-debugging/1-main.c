c1f9d384d726468130c607e8a1b0b9dc7326b2a7
osasagain <osasogbokhun@gmail.come>
2022-09-16 08:05:45 -0700

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: infinit loop
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+
+/*
+ * while (i < 10)
+ * {
+ * 	putchar(i);
+ * }
+ */
+	printf("infinit loop avoided!\\o/\n");
+
+	return(0);
+}
