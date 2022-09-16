73d12692e5dec31086926cbb267036cf7fe0b71a
osasagain <osasogbokhun@gmail.come>
2022-09-15 18:57:05 -0700

    Task 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,16 +8,16 @@
  * Return: 0
  */
 
-void print_most_numbers(void)
+void print_most_numbers(void
 {
-
 	int n;
-
+	
 	for (n = 48; n < 58; n++)
 	{
 		if ((n == 50) || (n == 52))
 		{
 			continue;
+
 		}
 		putchar(n);
 	}
