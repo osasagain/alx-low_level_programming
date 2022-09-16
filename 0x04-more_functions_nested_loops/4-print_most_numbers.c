fb59a51c837ca764e9f6a1fdafe31b61e2644694
osasagain <osasogbokhun@gmail.come>
2022-09-15 18:37:48 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,20 +3,23 @@
 /**
  * main - entry point
  *
- * Desription: function that print all numbers from 0 to 9
+ * Decription: Print number 0 to 9
  *
- * Return: 0 
+ * Return: 0
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 
 	int n;
 
 	for (n = 48; n < 58; n++)
 	{
+		if ((n == 50) || (n == 52))
+		{
+			continue;
+		}
 		putchar(n);
 	}
 	putchar(10);
-
 }
