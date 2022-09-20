e4293d64676d1defc12e03eecb2912049d891803
osasagain <osasogbokhun@gmail.come>
2022-09-20 14:44:34 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse order
+ * @s: string in reverse
+ * Return: nothing
+ */
+
+void print_rev(char *s);
+{
+	int len = strlen (s);
+
+	while (len ==)
+		putchar(*(s + len));
+	putchar(10);
+}
