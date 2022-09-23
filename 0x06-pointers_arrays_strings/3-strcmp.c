970a1bc9ea50c353fc78b4a4c116afc5cf8f90ad
osasagain <osasogbokhun@gmail.come>
2022-09-22 20:40:01 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,29 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i = 0, diff = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (1)
 	{
-		if (s1[i] != s2[i] - s2[i]);
-		i++;
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-	if (s1[i] == s2[i])
-		return (0);
-
-	return (s1[i] - s2[i]);
+	return (diff);
 }
