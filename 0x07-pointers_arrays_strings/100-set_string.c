5a553f1c91d7b818160de57e62ee216649207220
osasagain <osasogbokhun@gmail.come>
2022-09-27 15:15:25 -0700

    Task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string = set the value of a string
+ * @s: pointer to change
+ * @to: string to change the point
+ *
+ * Return: void
+ */
+
+
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
