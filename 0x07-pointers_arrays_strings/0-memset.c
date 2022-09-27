40a22c0ca8f63e27e7ad8f0a31d5b184c5e70bc2
osasagain <osasogbokhun@gmail.come>
2022-09-27 12:28:06 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- * main - entry pint 
  * _memset - file a program with a constant byte
  * @s: the memory block
  * @b: char to be used
@@ -14,7 +13,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	while (n)
 	{
-		s[n - 1] = b;
+		s[n - 1] =b;
 		n--;
 	}
 	return (s);
