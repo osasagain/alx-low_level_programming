f8d89deb3964c148e4058fac7eaadc92ba2acc76
osasagain <osasogbokhun@gmail.come>
2022-09-28 18:40:17 -0700

    Task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,7 +4,7 @@
  * factorial = return the factorial of a given number
  * @n: number to be used
  *
- * Return: the factorial of thw number
+ * Return: the factorial of the number
  */
 
 int factorial(int n)
