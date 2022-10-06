8cd73da1315af7d2a8a908bb3cb6ed706a9084c5
osasagain <osasogbokhun@gmail.come>
2022-10-06 13:16:50 -0700

    first commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -11,5 +8,8 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *malloc_checked(unsigned int b);
 int _putchar(char c);
+void _putchar(char str);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
 
 #endif /* MAIN_H */
