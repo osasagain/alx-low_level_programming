f2a6bf95440b15d9e991391992c3c7ccab45147b
osasagain <osasogbokhun@gmail.come>
2022-10-12 11:42:01 -0700

    op function

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,35 +1,179 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include "3-calc.h"
 
-/**
- * get_op_func - Selects correct function based on @s
- * @s: operator
- * Return: The pointer to the operator or
- * NULL if not found
- */
 
-int (*get_op_func(char *s))(int, int)
+
+/**
+ *
+ * * op_add - Adds two numbers @a and @b
+ *
+ * *
+ *
+ * * @a: A number
+ *
+ * *
+ *
+ * * @b: A number
+ *
+ * *
+ *
+ * * Return: Sum of @a and @b
+ *
+ * */
+
+
+
+int op_add(int a, int b)
+
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
-	
-	i = 0;
-	
-	while (i < 5)
-	{
-		if (*(ops[i].op) == *s)
-			return (ops[i].f);
-		
-		i++;
-	}
-	
-	return (ops[i].f);
+
+		return (a + b);
+
+}
+
+
+
+/**
+ *
+ * * op_sub - Calculates the difference of @c and @b
+ *
+ * *
+ *
+ * * @a: A number
+ *
+ * *
+ *
+ * * @b: A number
+ *
+ * *
+ *
+ * * Return: Difference of @a and @b
+ *
+ * */
+
+
+
+int op_sub(int a, int b)
+
+{
+
+		return (a - b);
+
+}
+
+
+
+/**
+ *
+ * * op_mul - Multiplies two numbers
+ *
+ * *
+ *
+ * * @a: A number
+ *
+ * *
+ *
+ * * @b: A number
+ *
+ * *
+ *
+ * * Return: Product of @a and @b
+ *
+ * */
+
+
+
+int op_mul(int a, int b)
+
+{
+
+		return (a * b);
+
+}
+
+
+
+/**
+ *
+ * * op_div - Divides @a by @b
+ *
+ * *
+ *
+ * * @a: A number
+ *
+ * *
+ *
+ * * @b: A number
+ *
+ * *
+ *
+ * * Return: Result of the division of @a by @b without remainder
+ *
+ * */
+
+
+
+int op_div(int a, int b)
+
+{
+
+		if (b == 0)
+
+				{
+
+							printf("Error\n");
+
+									exit(100);
+
+										}
+
+
+
+			return (a / b);
+
+}
+
+
+
+/**
+ *
+ * * op_mod - Calculates the remainder of @a divided by @b
+ *
+ * *
+ *
+ * * @a: A number
+ *
+ * *
+ *
+ * * @b: A number
+ *
+ * *
+ *
+ * * Return: Remainder of @a divider by @b
+ *
+ * */
+
+
+
+int op_mod(int a, int b)
+
+{
+
+		if (b == 0)
+
+				{
+
+							printf("Error\n");
+
+									exit(100);
+
+										}
+
+
+
+			return (a % b);
+
 }
