20b298996b0716e1b504cc1ec5befde4f7b1f725
osasagain <osasogbokhun@gmail.come>
2022-10-12 10:39:23 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name -  function that print name
+ * @name: name pointer
+ * @f: function pointer
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
